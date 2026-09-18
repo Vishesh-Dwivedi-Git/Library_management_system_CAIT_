5784a5f6319a03fbd97237d65a98b38225ed7537
mytoken <149029169+la-rosilva@users.noreply.github.com>
2026-09-18 17:01:31 +0530

    Update BookManagement.java

--- src/service/BookManagement.java
+++ src/service/BookManagement.java
@@ -7,45 +7,42 @@ import java.util.List;
 
 public class BookManagement {
 
-    private List<Book> books=new ArrayList<>();
+    private List<Book> books = new ArrayList<>();
 
-    public void addBook(Book book) {
+    public void addBook(Book book) throws DuplicateBookException {
+        if (searchByIsbn(book.getIsbn()) != null) {
+            throw new DuplicateBookException(book.getIsbn());
+        }
         books.add(book);
     }
 
-    
-    public boolean updateBook(String isbn, String title, String author,
-                              String publisher, int yearPublished,
-                              String genre, int totalCopies) {
+    public void updateBook(String isbn, String title, String author,
+                            String publisher, int yearPublished,
+                            String genre, int totalCopies) throws BookNotFoundException {
 
         Book book = searchByIsbn(isbn);
 
-        if (book != null) {
-            book.setTitle(title);
-            book.setAuthor(author);
-            book.setPublisher(publisher);
-            book.setYearPublished(yearPublished);
-            book.setGenre(genre);
-            book.setTotalCopies(totalCopies);
-
-            return true;
+        if (book == null) {
+            throw new BookNotFoundException(isbn);
         }
 
-        return false;
+        book.setTitle(title);
+        book.setAuthor(author);
+        book.setPublisher(publisher);
+        book.setYearPublished(yearPublished);
+        book.setGenre(genre);
+        book.setTotalCopies(totalCopies);
     }
 
-   
-    public boolean deleteBook(String isbn) {
+    public void deleteBook(String isbn) throws BookNotFoundException {
         Book book = searchByIsbn(isbn);
 
-        if (book != null) {
-            books.remove(book);
-            return true;
+        if (book == null) {
+            throw new BookNotFoundException(isbn);
         }
 
-        return false;
+        books.remove(book);
     }
-
 
     public List<Book> searchByTitle(String title) {
         List<Book> results = new ArrayList<>();
@@ -59,7 +56,6 @@ public class BookManagement {
         return results;
     }
 
-   
     public List<Book> searchByAuthor(String author) {
         List<Book> results = new ArrayList<>();
 
@@ -72,7 +68,6 @@ public class BookManagement {
         return results;
     }
 
-   
     public Book searchByIsbn(String isbn) {
         for (Book book : books) {
             if (book.getIsbn().equalsIgnoreCase(isbn)) {
@@ -83,7 +78,6 @@ public class BookManagement {
         return null;
     }
 
-  
     public void displayAllBooks() {
         if (books.isEmpty()) {
             System.out.println("No books available.");
@@ -95,15 +89,14 @@ public class BookManagement {
         }
     }
 
-  
-    public boolean checkAvailability(String isbn) {
+    public boolean checkAvailability(String isbn) throws BookNotFoundException {
         Book book = searchByIsbn(isbn);
 
-        if (book != null) {
-            return book.isAvailable();
+        if (book == null) {
+            throw new BookNotFoundException(isbn);
         }
 
-        return false;
+        return book.isAvailable();
     }
 
     public List<Book> getBooks() {
